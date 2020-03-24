0420a60983a22f3c7a7a500bf94ec0018203c0ce
roel <roel@DESKTOP-1H2RIQ9>
2020-03-24 10:27:29 +0100

    lobby

--- Submarine.Web/Controllers/HomeController.cs
+++ Submarine.Web/Controllers/HomeController.cs
@@ -28,11 +28,6 @@ namespace Submarine.Web.Controllers
             return View();
         }
 
-        public IActionResult Lobby()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
