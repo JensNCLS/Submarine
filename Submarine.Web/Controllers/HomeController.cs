8bc533da7a266868134992ee10a359117f215aeb
roel <roel@DESKTOP-1H2RIQ9>
2020-03-24 10:35:45 +0100

    quit rename

--- Submarine.Web/Controllers/HomeController.cs
+++ Submarine.Web/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace Submarine.Web.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Quit()
         {
             return View();
         }
