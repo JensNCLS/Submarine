63817ed4e9acdf1cbbf7e2d3cef1ad412f64b9cf
roel <roel@DESKTOP-1H2RIQ9>
2020-04-16 14:51:27 +0200

    grid changes

---
+++ Submarine.Web/Models/ShipModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Submarine.Web.Models
+{
+    public class ShipModel
+    {
+        public int shipPlaces { get; set; }
+    }
+}
